b6c3b9f06233efceb584240759bc0d30e8dacb88
Сергей <serega.maksimenko@gmail.com>
2021-06-15 17:55:36 +0600

    create card for column

--- src/components/BoardColumn.js
+++ src/components/BoardColumn.js
@@ -1,25 +1,55 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { removeBoard } from '../app/reducers/Boards';
+import ColumnCard from './ColumnCard';
 
 export default function BoardColumn(props) {
   const [cards, setCards] = useState([]);
+  const [newCardTitle, setNewCardTitle] = useState('');
+  const [isOpenAddCard, setIsAddOpenCard] = useState(false);
   const data = props.data;
   const dispatch = useDispatch();
+  function addCard() {
+    if (newCardTitle.length > 0) {
+      setCards((prevCards) => prevCards.concat({ text: newCardTitle }));
+      setNewCardTitle('');
+    }
+  }
+
   return (
     <div className={props.className}>
+      {/* header */}
       <div>
         <p>{data.title}</p>
         <button onClick={() => dispatch(removeBoard(data.id))}>X</button>
       </div>
-      <ul>{cards.length > 0 && cards.map((item) => <li>{item.text}</li>)}</ul>
-      <button
-        onClick={() => {
-          setCards((prevCards) => prevCards.concat({ text: 'test' }));
-        }}
-      >
-        Add card
-      </button>
+      {/* cards */}
+      <ul>
+        {cards.length > 0 &&
+          cards.map((item, index) => <ColumnCard key={index} data={item} />)}
+      </ul>
+      {/* footer */}
+      <div>
+        {!isOpenAddCard && (
+          <button onClick={() => setIsAddOpenCard(true)}>
+            Add one more card
+          </button>
+        )}
+        {isOpenAddCard && (
+          <div>
+            <input
+              type="text"
+              value={newCardTitle}
+              onChange={(e) => setNewCardTitle(e.target.value)}
+              placeholder="Input title card"
+            />
+            <div>
+              <button onClick={addCard}>Add card</button>
+              <button onClick={() => setIsAddOpenCard(false)}>X</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/ColumnCard.js
@@ -0,0 +1,4 @@
+export default function ColumnCard(props) {
+  const data = props.data;
+  return <li>{data.text}</li>;
+}
